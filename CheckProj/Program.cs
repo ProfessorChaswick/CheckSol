e77059c5779b89226a8cf10b8bddd926c50e9ed7
Charles Hildebrant <ceh2624@gmail.com>
2022-09-15 22:42:06 -0400

    Add project files.

---
+++ CheckProj/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static bool CheckEquality(object a, object b)
+    {
+        TypeCode aType = Type.GetTypeCode(a.GetType());
+        TypeCode bType = Type.GetTypeCode(b.GetType());
+        Console.WriteLine("a is type {0}", aType);
+        Console.WriteLine("b is type {0}", bType);
+        Console.WriteLine("+++++++++++++++++++");
+        Console.WriteLine("a = {0} ", a);
+        Console.WriteLine("b = {0} ", b);
+        Console.WriteLine("+++++++++++++++++++");
+        Console.WriteLine("+++++++++++++++++++");
+        if (aType.Equals(bType) && a.Equals(b)) 
+            return true;
+        else
+            return false;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Result: " + CheckEquality("11", "11"));
+    }
+}
